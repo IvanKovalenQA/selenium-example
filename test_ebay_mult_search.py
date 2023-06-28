9a4f127145018f85f319a978f96c424ba6c9495e
Ivan <IvanKovalenQA@gmail.com>
2023-06-28 03:52:33 +0300

    test_multiple_search.py comment added

--- test_ebay_mult_search.py
+++ test_ebay_mult_search.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
     ('2024', 'Lexus', 'RX350', 'Luxury Sport Utility 4-Door'),
     ('1896', 'Duryea', 'Duryea', 'Base')
 ])
+#comment
 @pytest.mark.regressiontest6
 def test_ebay_search_motors_multiple(browser, year, make, model, trim):
     base_url = 'https://www.ebay.com/'
